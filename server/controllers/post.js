94df54087491bc670858220c78fecbfc175d5ae8
lukasz kopyszko <lukaszkopyszko98@gmail.com>
2020-01-29 18:52:02 +0100

    Adding posts - server side. Tested with postman, it works.

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -1,5 +1,28 @@
+const Post = require('../models/post');
+const slugify = require('slugify');
+
+
+
+
 exports.create = (req, res) => {
-    res.json({
-        data: 'hello from controllers'
+    // console.log(req.body);
+    const { title, content, user } = req.body
+    const slug = slugify(title) // My Post my-post
+        // validate
+    switch (true) {
+        case !title:
+            return res.status(400).json({ error: 'Title is required' });
+            break;
+        case !content:
+            return res.status(400).json({ error: 'Content is required' });
+            break;
+    }
+    // create new post
+    Post.create({ title, content, user, slug }, (err, post) => {
+        if (err) {
+            console.log(err)
+            res.status(400).json({ error: 'Duplicate post. Try another title' })
+        }
+        res.json(post);
     });
 };
