d46d66c44331b379830270bd679ac4d443c5a0cb
lukasz kopyszko <lukaszkopyszko98@gmail.com>
2020-02-04 16:40:26 +0100

    Read,Update, Delete - DONE

--- client/src/Create.jsx
+++ client/src/Create.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import axios from 'axios';
+import Nav from './Nav';
 const Create = () => {
     // state
     const [state, setState] = useState({
@@ -35,7 +36,9 @@ const Create = () => {
     };
 
     return(
-        <div className="container p-5">
+        <div className="container pb-5">
+            <Nav/>
+            <br/>
             <h1>Create Post</h1>
             <br/>
             <form onSubmit={handleSubmit}>
